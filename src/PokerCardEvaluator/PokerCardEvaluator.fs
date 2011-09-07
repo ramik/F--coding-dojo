d2bb4c17d299c59cfdab41355491fdfd67aa0822
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-09-07 12:48:19 +0300

    Deck types and full card deck initialization in place, along tests

--- src/PokerCardEvaluator/PokerCardEvaluator.fs
+++ src/PokerCardEvaluator/PokerCardEvaluator.fs
@@ -1,3 +1,12 @@
 ﻿module PokerCardEvaluator
+open Microsoft.FSharp.Reflection 
 
-let dummyForTest = false
+type Suit = Heart | Diamond | Club | Spade
+type Card = | Ace of Suit | King of Suit | Queen of Suit | Jack of Suit
+            | ValueCard of int * Suit
+
+let GetFullCardsForSuit suit = seq { yield Ace(suit); yield King(suit); yield Queen(suit); yield Jack(suit)
+                                     for value in [2..10] do yield ValueCard(value, suit); }
+
+let GetDeck = Seq.map (fun c -> GetFullCardsForSuit c) >> Seq.concat
+let Deck = GetDeck [Heart;Diamond;Club;Spade]
--- test/PokerCardEvaluatorTests/PokerCardEvaluationTest.fs
+++ test/PokerCardEvaluatorTests/PokerCardEvaluationTest.fs
@@ -3,5 +3,21 @@
 open Xunit
 open PokerCardEvaluator
 
+let fullSuiteOf = function | x -> [Ace(x);King(x);Queen(x);Jack(x);ValueCard(2, x); 
+                                   ValueCard(3, x);ValueCard(4, x);ValueCard(5, x);ValueCard(6, x);
+                                   ValueCard(7, x);ValueCard(8, x);ValueCard(9, x);ValueCard(10, x)]
+
 [<Fact>]
-let SimpleTestingDummy() = Assert.False dummyForTest
+let DeckContainsAllCards() = Assert.Equal (52, (Seq.length Deck))
+
+[<Fact>]
+let DeckContainsAllHearts() = Assert.Equal (fullSuiteOf Heart, Deck |> Seq.take 13 |> Seq.toList)
+          
+[<Fact>]
+let DeckContainsAllDiamonds() = Assert.Equal (fullSuiteOf Diamond, Deck |> Seq.skip 13 |> Seq.take 13 |> Seq.toList)     
+
+[<Fact>]
+let DeckContainsAllClubs() = Assert.Equal (fullSuiteOf Club, Deck |> Seq.skip 26 |> Seq.take 13 |> Seq.toList)     
+
+[<Fact>]
+let DeckContainsAllSpapes() = Assert.Equal (fullSuiteOf Spade, Deck |> Seq.skip 39 |> Seq.take 13 |> Seq.toList)     
