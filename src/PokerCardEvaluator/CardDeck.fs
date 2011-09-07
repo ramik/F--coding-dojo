9f3895cc50bbb5504149e6074f5dc3d1f100f08f
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-09-08 00:02:54 +0300

    Added tostring to face cards as well

--- src/PokerCardEvaluator/CardDeck.fs
+++ src/PokerCardEvaluator/CardDeck.fs
@@ -7,8 +7,13 @@ type Suit = Heart | Diamond | Club | Spade
 type Card =
     | Ace of Suit | King of Suit | Queen of Suit | Jack of Suit
     | ValueCard of int * Suit
-    override this.ToString() = match this with | ValueCard(a, b) -> String.Format ("{0} of {1}", a, b)
-                                               | _ -> "foo"
+    override this.ToString() = 
+                        let Format a b = String.Format ("{0} of {1}", a, b)
+                        match this with | ValueCard(a, b) -> Format a b
+                                        | Ace(x) -> Format "Ace" x
+                                        | King(x) -> Format "King" x
+                                        | Queen(x) -> Format "Queen" x
+                                        | Jack(x) -> Format "Jack" x
 
 let GetFullCardsForSuit suit = seq { yield Ace(suit); yield King(suit); yield Queen(suit); yield Jack(suit)
                                      for value in [2..10] do yield ValueCard(value, suit); }
