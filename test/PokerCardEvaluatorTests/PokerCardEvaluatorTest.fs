71ff15a2278a4adf8d55dfe130d4e09a24a44bb4
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-09-08 12:05:15 +0300

    Reformatted test code

--- test/PokerCardEvaluatorTests/PokerCardEvaluatorTest.fs
+++ test/PokerCardEvaluatorTests/PokerCardEvaluatorTest.fs
@@ -5,9 +5,11 @@ open Cards
 open PokerCardEvaluator
 
 [<Fact>]
-let DeckGetsShuffledWithSeed1() = Assert.Equal ([ValueCard(5, Club); ValueCard(8, Heart); ValueCard(9, Club); ValueCard(6, Diamond);ValueCard(6, Heart);King(Heart)], 
-                                                ShuffleCards (new System.Random(1)) Deck |> Seq.take 6 |> Seq.toList)
+let DeckGetsShuffledWithSeed1() = 
+       let expected = [ValueCard(5, Club); ValueCard(8, Heart); ValueCard(9, Club); ValueCard(6, Diamond);ValueCard(6, Heart);King(Heart)]
+       Assert.Equal (expected, ShuffleCards (new System.Random(1)) Deck |> Seq.take 6 |> Seq.toList)
 
 [<Fact>]
-let DeckGetsShuffledWithSeed3() = Assert.Equal ([Ace(Diamond); ValueCard(5, Club); ValueCard(10, Spade); ValueCard(8, Diamond); King(Diamond); Jack(Diamond)], 
-                                                ShuffleCards (new System.Random(3)) Deck |> Seq.take 6 |> Seq.toList)
+let DeckGetsShuffledWithSeed3() = 
+       let expected = [Ace(Diamond); ValueCard(5, Club); ValueCard(10, Spade); ValueCard(8, Diamond); King(Diamond); Jack(Diamond)]
+       Assert.Equal (expected, ShuffleCards (new System.Random(3)) Deck |> Seq.take 6 |> Seq.toList)
