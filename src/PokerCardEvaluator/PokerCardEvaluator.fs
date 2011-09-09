a978b72b69464e411e9cc44c427a82cbecc2295f
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-09-09 13:26:20 +0300

    Added pattern matching for pairs

--- src/PokerCardEvaluator/PokerCardEvaluator.fs
+++ src/PokerCardEvaluator/PokerCardEvaluator.fs
@@ -9,4 +9,25 @@ let ShuffleCards (random : Random) = Seq.map (fun c -> (random.Next(), c)) >>
 
 let GetShuffledDeck deck = ShuffleCards (new System.Random()) deck
 let GetHand = GetShuffledDeck >> Seq.take 5
-GetHand Deck |> ignore
+
+type Shark = int
+type Duplicates = { value : int; shark : Shark }
+type Hand = | OnlyShark of Shark 
+            | FourOfKind of Duplicates
+            | Pair of Duplicates
+            | TwoPairs of Hand * Hand
+            | ThreeOfKind of Duplicates
+            | FullHouse of Hand * Hand
+
+let EvaluatePairs (hand : seq<Card>) =
+       let sortedpairs = hand |> Seq.countBy (fun c -> c.FaceValue) |> Seq.sortBy (fun (b, a) -> -a * 20 - b) |> Seq.toList
+       match sortedpairs with 
+             | (a, b) :: (c, d) :: _ when b = 2 -> Some(Pair({ value = a; shark = c }))
+             | _ -> None
+
+let EvaluateHand (hand : seq<Card>) = 
+    let eval = EvaluatePairs hand
+    match eval with | Some(x) -> x
+                    | None -> raise (NotImplementedException("only ponies"))                                  
+
+GetHand Deck |> EvaluateHand |> ignore
--- test/PokerCardEvaluatorTests/PokerCardEvaluatorTest.fs
+++ test/PokerCardEvaluatorTests/PokerCardEvaluatorTest.fs
@@ -13,3 +13,10 @@ let DeckGetsShuffledWithSeed1() =
 let DeckGetsShuffledWithSeed3() = 
        let expected = [Ace(Diamond); ValueCard(5, Club); ValueCard(10, Spade); ValueCard(8, Diamond); King(Diamond); Jack(Diamond)]
        Assert.Equal (expected, ShuffleCards (new System.Random(3)) Deck |> Seq.take 6 |> Seq.toList)
+
+[<Fact>]
+let FindsPairFromHand() = 
+   Assert.Equal (Pair({value = 14; shark = 11}), EvaluateHand [Ace(Diamond); ValueCard(5, Club); Ace(Spade); ValueCard(8, Diamond); Jack(Diamond)])
+   Assert.Equal (Pair({value = 11; shark = 14}), EvaluateHand [Jack(Diamond); ValueCard(5, Club); Ace(Spade); ValueCard(8, Diamond); Jack(Club)])
+
+    
